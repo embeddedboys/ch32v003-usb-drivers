b9869a97d30bf2c9e3688993e72c65e5ab0ee5f0
Wooden Chair <hua.zheng@embeddedboys.com>
2026-01-27 20:51:34 +0800

    vendor: add more gpio requests
    Signed-off-by: Wooden Chair <hua.zheng@embeddedboys.com>

--- vendor/vendor.c
+++ vendor/vendor.c
@@ -44,11 +44,22 @@ void handle_gpio_out_request(u16 cmd, u16 data)
 	case V003_GPIO_SET:
 		funDigitalWrite(idx, state);
 		break;
+	case V003_GPIO_REQUEST:
+	case V003_GPIO_FREE:
+	case V003_GPIO_DIRECTION_INPUT:
+		funPinMode(idx, GPIO_Speed_In | GPIO_CNF_IN_PUPD);
+		break;
+	case V003_GPIO_DIRECTION_OUTPUT:
+		funPinMode(idx, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP);
+		funDigitalWrite(idx, state);
+		break;
 	default:
 		break;
 	}
 }
 
+// #define funPinMode( pin, mode ) { GpioOf(pin)->CFGLR = (GpioOf(pin)->CFGLR & (~(0xf<<(4*((pin)&0xf))))) | ((mode)<<(4*((pin)&0xf))); }
+#define funPinGetMode(pin) ((GpioOf(pin)->CFGLR >> (4 * ((pin) & 0xF))) & 0xF)
 u32 handle_gpio_in_request(u16 cmd, u16 data)
 {
 	u8 idx = data >> 8;
@@ -59,6 +70,9 @@ u32 handle_gpio_in_request(u16 cmd, u16 data)
 	switch (cmd) {
 	case V003_GPIO_GET:
 		state = funDigitalRead(idx);
+		break;
+	case V003_GPIO_GET_DIRECTION:
+		state = (funPinGetMode(idx) & 0x03) == 0x00 ? 1 : 0;
 		break;
 	default:
 		break;
@@ -104,7 +118,6 @@ int main()
 	SystemInit();
 
 	funGpioInitAll();
-	funPinMode(PC0, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP);
 
 	usb_setup();
 
